cb77be6b02f7a272512019f376139d6cd7180d6d
Nilesh <autotraznilesh@gmail.com>
2018-04-10 15:05:04 +0530

    first commit

---
+++ ads.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+    <title>Status</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+	.block {
+		display: none;
+	}
+	
+</style>
+<!-- script for post-login interstitial [should be placed in the head 
+section]-->
+<!--<script>
+var chnlId=0; var pubId=171; var pId="jupicom"; var pName="[placeapid/hotspotname here]"; var cBuster=new Date().getTime();
+var spidIUrl=('https:' == document.location.protocol ? 'https://' : 'http://') + 'wifi.roamm.com/basics/'+pId;
+var spidIScr='<scr'+'ipt src="'+spidIUrl+'/_post.js?cbuster='+cBuster+'"/></scr'+'ipt>';
+document.write(spidIScr);
+</script> -->
+
+<!-- script for prelogin interstitial [should be placed in the head 
+section]-->
+<!--<script>
+var chnlId=0; var pubId=171; var pId="jupicom"; var pName="[placeapid/hotspotname here]"; var cBuster=new Date().getTime();
+var spidIUrl=('https:' == document.location.protocol ? 'https://' : 'http://') + 'wifi.roamm.com/basics/'+pId;
+var spidIScr='<scr'+'ipt src="'+spidIUrl+'/_pre.js?cbuster='+cBuster+'"/></scr'+'ipt>';
+document.write(spidIScr);
+</script> -->
+
+
+</head>
+
+<body>
+<div id="ad" style="width:728px; margin:0 auto;" >
+<div id="ad" style="width:100%;height:66px;" >
+   <iframe
+      src="http://matthewjamestaylor.com/responsive-ads/ad.html"
+      border="0"
+      scrolling="no"
+      allowtransparency="true"
+      width="100%"
+      height="100%"
+      style="border:0;">
+   </iframe>
+</div>
+</div>
+</body>
+</html>
